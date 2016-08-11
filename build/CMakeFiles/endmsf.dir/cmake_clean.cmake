54719c03d3a085bbe05f524a1d4abde4d6cc66cd
canyou2014 <599091274@qq.com>
2016-08-11 17:54:25 +0800

    add all

---
+++ build/CMakeFiles/endmsf.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/endmsf.dir/src/measurement.cpp.o"
+  "CMakeFiles/endmsf.dir/src/position_sensor.cpp.o"
+  "CMakeFiles/endmsf.dir/src/SSF_Core.cpp.o"
+  "CMakeFiles/endmsf.dir/src/state.cpp.o"
+  "../lib/libendmsf.pdb"
+  "../lib/libendmsf.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/endmsf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
